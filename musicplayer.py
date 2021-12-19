6a1d948fec610f1ccf349a419e945a3256491f6d
riyaz0105 <86203310+riyaz0105@users.noreply.github.com>
2021-12-19 08:18:36 +0530

    updated music player layout

--- musicplayer.py
+++ musicplayer.py
@@ -1,3 +1,4 @@
+
 # Importing Required Modules & libraries
 from tkinter import *
 import os
@@ -19,45 +20,45 @@ class MusicPlayer(object):
   def __init__(self,root,emotionStr):
     self.root = root
     # Title of the window
-    self.root.title("Music Player")
+    self.root.title("EMO Player")
     # Window Geometry
-    self.root.geometry("1000x200+200+200")
+    self.root.geometry("1280x200+200+200")
     # Declaring track Variable
     self.track = StringVar()
     # Declaring Status Variable
     self.status = StringVar()
     # Creating Track Frame for Song label & status label
-    trackframe = LabelFrame(self.root,text="Song Track",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
-    trackframe.place(x=0,y=0,width=620,height=100)
+    trackframe = LabelFrame(self.root,text="Song Track",font=("times new roman",10,"bold"),bg="#0C090A",fg="white",bd=5,relief=GROOVE)
+    trackframe.place(x=0,y=0,width=700,height=100)
     # Inserting Song Track Label
-    songtrack = Label(trackframe,textvariable=self.track,width=20,font=("times new roman",24,"bold"),bg="grey",fg="gold").grid(row=0,column=0,padx=10,pady=5)
+    songtrack = Label(trackframe,textvariable=self.track,width=25,font=("times new roman",18,"bold"),bg="#0C090A",fg="#DC143C").grid(row=0,column=0,padx=10,pady=5)
     # Inserting Status Label
-    trackstatus = Label(trackframe,textvariable=self.status,font=("times new roman",18,"bold"),bg="grey",fg="gold").grid(row=0,column=1,padx=5,pady=5)
+    trackstatus = Label(trackframe,textvariable=self.status,font=("times new roman",18,"bold"),bg="#0C090A",fg="#DC143C").grid(row=0,column=1,padx=5,pady=5)
     # Creating Button Frame
-    buttonframe = LabelFrame(self.root,text="Control Panel",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
-    buttonframe.place(x=0,y=100,width=620,height=100)
+    buttonframe = LabelFrame(self.root,text="Control Panel",font=("times new roman",15,"bold"),bg="#0C090A",fg="white",bd=5,relief=GROOVE)
+    buttonframe.place(x=0,y=100,width=700,height=100)
     # Inserting Play Button
-    playbtn = Button(buttonframe,text="PLAY",command=self.playsong,width=6,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="gold").grid(row=0,column=0,padx=10,pady=5)
+    playbtn = Button(buttonframe,text="PLAY",command=self.playsong,width=6,height=1,font=("times new roman",16,"bold"),fg="white",bg="#DC143C").grid(row=0,column=0,padx=10,pady=5)
     # Inserting Pause Button
-    playbtn = Button(buttonframe,text="PAUSE",command=self.pausesong,width=8,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="gold").grid(row=0,column=1,padx=10,pady=5)
+    playbtn = Button(buttonframe,text="PAUSE",command=self.pausesong,width=6,height=1,font=("times new roman",16,"bold"),fg="white",bg="#DC143C").grid(row=0,column=1,padx=10,pady=5)
     # Inserting Unpause Button
-    playbtn = Button(buttonframe,text="SHUFFLE",command=self.shufflesong,width=10,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="gold").grid(row=0,column=2,padx=10,pady=5)
+    playbtn = Button(buttonframe,text="SHUFFLE",command=self.shufflesong,width=7,height=1,font=("times new roman",16,"bold"),fg="white",bg="#DC143C").grid(row=0,column=2,padx=10,pady=5)
     # Inserting Stop Button
-    playbtn = Button(buttonframe,text="STOP",command=self.stopsong,width=6,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="gold").grid(row=0,column=3,padx=10,pady=5)
-    playbtn = Button(buttonframe,text="NEXT",command=self.nextsong,width=6,height=1,font=("times new roman",16,"bold"),fg="navyblue",bg="gold").grid(row=0,column=4,padx=10,pady=5)
+    playbtn = Button(buttonframe,text="STOP",command=self.stopsong,width=6,height=1,font=("times new roman",16,"bold"),fg="white",bg="#DC143C").grid(row=0,column=3,padx=10,pady=5)
+    playbtn = Button(buttonframe,text="NEXT",command=self.nextsong,width=6,height=1,font=("times new roman",16,"bold"),fg="white",bg="#DC143C").grid(row=0,column=4,padx=10,pady=5)
     # Creating Playlist Frame
-    songsframe = LabelFrame(self.root,text="Song Playlist",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
+    songsframe = LabelFrame(self.root,text="Song Playlist",font=("times new roman",15,"bold"),bg="#0C090A",fg="white",bd=5,relief=GROOVE)
     songsframe.place(x=600,y=0,width=400,height=200)
     # Inserting scrollbar
     scrol_y = Scrollbar(songsframe,orient=VERTICAL)
     # Inserting Playlist listbox
-    self.playlist = Listbox(songsframe,yscrollcommand=scrol_y.set,selectbackground="gold",selectmode=SINGLE,font=("times new roman",12,"bold"),bg="silver",fg="navyblue",bd=5,relief=GROOVE)
+    self.playlist = Listbox(songsframe,yscrollcommand=scrol_y.set,selectbackground="#DC143C",selectmode=SINGLE,font=("times new roman",12,"bold"),bg="Black",fg="White",bd=5,relief=GROOVE)
     # Applying Scrollbar to listbox
     scrol_y.pack(side=RIGHT,fill=Y)
     scrol_y.config(command=self.playlist.yview)
     self.playlist.pack(fill=BOTH)
     # Changing Directory for fetching Songs
-    os.chdir(str(Path(__file__).parent.absolute())+"\songs\\"+emotionStr+"\\")
+    os.chdir(str(Path(__file__).parent.absolute())+"/songs/"+emotionStr+"/")
     # Fetching Songs
     songtracks = os.listdir()
     self.songtracks = songtracks
@@ -68,7 +69,7 @@ class MusicPlayer(object):
       ranSong = random.choice(self.songtracks)
       self.pos = self.songtracks.index(ranSong)
       self.track.set(ranSong)
-      self.status.set("-Playing "+emotionStr)
+      self.status.set(emotionStr)
       Media = Instance.media_new(ranSong)
       player.set_media(Media)
       player.play()
@@ -138,6 +139,5 @@ class MusicPlayer(object):
 # Passing Root to MusicPlayer Class
 # Root Window Looping
 #root.mainloop()
-
 
 
